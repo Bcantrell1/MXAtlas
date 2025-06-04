02949d8b0d031d3c528d6a6b5ab88a7cf598f8c0
Bcantrell1_WSL <bribri546@gmail.com>
2025-06-04 15:35:07 -0700

    Add create track route | find by Id | basic search by track name

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from database import SessionLocal, get_db
-from models import Track
+from models import NewTrack, Track
 
 app = FastAPI()
 
@@ -17,6 +17,36 @@ def get_tracks(type: str = None, country: str = None, difficulty: str = None, db
         if difficulty:
             query = query.filter(Track.difficulty == difficulty)
         return query.all()
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}") 
+
+@app.post("/track/")
+def create_track(track: NewTrack, db: Session = Depends(get_db)):
+	try:
+		db.add(track)
+		db.commit()
+		return {"message": "Track created successfully"}
+	except Exception as e:
+		print(f"Unexpected error: {e}")
+		raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}") 
+
+@app.get("/tracks/{track_id}")
+def get_track(track_id: int, db: Session = Depends(get_db)):
+    try:
+        track = db.query(Track).filter(Track.id == track_id).first()
+        if not track:
+            raise HTTPException(status_code=404, detail="Track not found")
+        return track
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}") 
+
+@app.get("/tracks/search")
+def search_tracks(query: str, db: Session = Depends(get_db)):
+    try:
+        tracks = db.query(Track).filter(Track.name.ilike(f"%{query}%")).all()
+        return tracks
     except Exception as e:
         print(f"Unexpected error: {e}")
         raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}") 
--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, JSON
 from database import Base
+from pydantic import BaseModel
 
 class Track(Base):
     __tablename__ = 'tracks'
@@ -17,3 +18,18 @@ class Track(Base):
     capacity = Column(Integer)
     established = Column(Integer)
     description = Column(String)
+
+class NewTrack(BaseModel):
+	name: str
+	location: str
+	country: str
+	type: str
+	difficulty: str
+	longitude: float
+	latitude: float
+	elevation: int
+	track_length: str
+	capacity: int
+	established: int
+	description: str
+ 
